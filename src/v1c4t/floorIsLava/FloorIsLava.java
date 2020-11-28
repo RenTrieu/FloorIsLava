8ba5782e8caed3f3a5664a079fff2c111393f910
recovery1 <recovery1@Leeks>
2020-11-27 16:24:52 -0800

    Added support for multiblock placements

--- src/v1c4t/floorIsLava/FloorIsLava.java
+++ src/v1c4t/floorIsLava/FloorIsLava.java
@@ -8,8 +8,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockMultiPlaceEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.Location;
@@ -62,7 +64,7 @@ public class FloorIsLava extends JavaPlugin
                            + "The FloorIsLava challenge is now activated.");
             for (Player player : getServer().getOnlinePlayers()) {
                 Location playerStartLoc = player.getLocation().clone();
-                playerStartLoc = playerStartLoc.subtract(0.0, 1.0, 0.0);
+                playerStartLoc = playerStartLoc.subtract(0.0, 0.1, 0.0);
                 this.placedBlockList.add(playerStartLoc.getBlock());
             }
             this.pluginState = true;
@@ -91,13 +93,28 @@ public class FloorIsLava extends JavaPlugin
     }
     
     /*
+     * When multiple blocks are placed with a place action, add each component
+     * to placedBlockList
+     */
+    @EventHandler
+    public void onBlockMultiPlace(BlockMultiPlaceEvent event) {
+        if (this.pluginState == true) {
+            for (BlockState bState : event.getReplacedBlockStates()) {
+                if (!(this.placedBlockList.contains(bState.getBlock()))) {
+                    this.placedBlockList.add(bState.getBlock());
+                }
+            }
+        }
+    }
+    
+    /*
      * When a player respawns, exempts the block underneath them
      * from killing them
      */
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Location spawnBlockLoc = event.getRespawnLocation().clone()
-                                      .subtract(0.0, 1.0, 0.0);
+                                      .subtract(0.0, 0.1, 0.0);
         if (!(this.placedBlockList.contains(spawnBlockLoc.getBlock()))
             && (this.pluginState == true)) {
             this.placedBlockList.add(spawnBlockLoc.getBlock());
@@ -111,7 +128,7 @@ public class FloorIsLava extends JavaPlugin
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Location destFloorLoc = event.getTo().clone().subtract(0.0, 1.0, 0.0);
+        Location destFloorLoc = event.getTo().clone().subtract(0.0, 0.1, 0.0);
         Block destBlock = destFloorLoc.getBlock();
         Material destMat = destBlock.getBlockData().getMaterial();
         if (!(this.placedBlockList.contains(destBlock))
